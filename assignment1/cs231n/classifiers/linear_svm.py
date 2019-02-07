a7dffef2f31d040137a2a89b4e478bfca42b9e48
hunhoon21 <hunhoon21@gmail.com>
2019-02-07 22:58:35 +0900

    SVM loss_naive dW 구하기

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,7 +35,8 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
-        
+        dW[:, j] += X[i,:].T
+        dW[:, y[i]] += X[i, :].T
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
